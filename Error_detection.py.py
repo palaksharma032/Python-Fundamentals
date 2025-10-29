d0fd738b10e7ed180d228cb64a5113a587efd0fd
Palak <palaksharma03210@gmail.com>
2025-10-29 20:24:20 +0530

    Python

---
+++ Error_detection.py.py
@@ -0,0 +1,9 @@
+butterflies = 10
+beetles = 12
+ladybugs = 20
+total= butterflies+beetles+ladybugs
+print('I caught '+str(butterflies)+'  butterflies!')
+print('I caught ' +str(beetles) + '  beetles!')
+print('I caught ' +str(ladybugs) + '  ladybugs !')
+
+print('I caught ' + str(total) + ' total bugs!')
